4c82cdf37c980d1956006ef6b0ed03dde0512b2b
Dav420I <david.arutyunyan.97@bk.ru>
2022-12-16 15:43:52 +0300

    init

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import {
 import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
 import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 
 
 const ScrollToTop = () => {
@@ -24,11 +26,13 @@ const ScrollToTop = () => {
 function App() {
   return <div className="App">
     <BrowserRouter>
+      <Navbar/>
       <ScrollToTop/>
       <Routes>
         <Route path= "/" element={<Home />} />
         <Route path= "item/:itemId"  element={<ItemDetails />} />
         <Route path= "checkout" element={<Checkout />} />
+        <Route path= "checkout/success" element={<Confirmation />} />
       </Routes>
     </BrowserRouter>
 
--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,5 +1,11 @@
-
-
+import {useDispatch, useSelector } from "react-redux";
+import {Badge, Box, IconButton} from "@mui/material";
+import {
+    PersonOutline,
+    ShoppingBagOutlined,
+    MenuOutlined,
+    SearchOutlined
+} from "@mui/icons-material";
 
 const Navbar = () => {
     return <div>
