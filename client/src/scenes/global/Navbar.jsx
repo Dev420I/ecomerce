f2f2deb4a390fed9d5d105a65430012f3a5fe39e
Dav420I <david.arutyunyan.97@bk.ru>
2022-12-16 16:58:34 +0300

    init

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -6,11 +6,65 @@ import {
     MenuOutlined,
     SearchOutlined
 } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom"
+import { shades } from "../../theme";
+
 
 const Navbar = () => {
-    return <div>
-        Navbar
-    </div>
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+
+    return (
+    <Box
+    display='flex'
+    alignItems='center'
+    width='100%'
+    height='60px'
+    backgroundColor="rgba(225, 225, 225, 0.95)"
+    color='black'
+    position='fixed'
+    top='0'
+    left='0'
+    zIndex='1'
+    >
+      <Box
+      width='80%'
+      margin='auto'
+      display='flex'
+      justifyContent='space-between'
+      alignItems='center'
+      >
+          <Box
+              onClick={() => navigate('/')}
+              sx={{'&:hover': {cursor: 'pointer'}}}
+              color={shades.secondary[500]}
+          >
+
+              ECOMMERCE
+          </Box>
+          <Box
+          display='flex'
+          justifyContent='space-between'
+          columnGap='20px'
+          zIndex='2'
+          >
+              <IconButton sx={{color: "black" }}>
+                  <SearchOutlined />
+              </IconButton>
+              <IconButton sx={{color: "black" }}>
+                  <PersonOutline />
+              </IconButton>
+              <IconButton sx={{color: "black" }}>
+                  <SearchOutlined />
+              </IconButton>
+              <IconButton sx={{color: "black" }}>
+                  <SearchOutlined />
+              </IconButton>
+          </Box>
+      </Box>
+    </Box>
+    )
 }
 
 export default Navbar
