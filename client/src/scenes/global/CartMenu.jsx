71e30d1919dcc3c5c2cb12e2c202b9f458ac49bd
Dav420I <david.arutyunyan.97@bk.ru>
2022-12-16 22:07:58 +0300

    init

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -1,58 +1,140 @@
 
-import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
-import { useSelector, useDispatch } from "react-redux";
-import CloseIcon from "@mui/icons-material/Close";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove"
-import styled from "@emotion/styled";
-import  { shades } from "../../theme";
-import {
-    decreaseCount,
-    increaseCount,
-    removeFromCart,
-    setIsCartOpen,
-} from "../../state";
-import { useNavigate } from "react-router-dom";
+    import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+    import { useSelector, useDispatch } from "react-redux";
+    import CloseIcon from "@mui/icons-material/Close";
+    import AddIcon from "@mui/icons-material/Add";
+    import RemoveIcon from "@mui/icons-material/Remove"
+    import styled from "@emotion/styled";
+    import  { shades } from "../../theme";
+    import {
+        decreaseCount,
+        increaseCount,
+        removeFromCart,
+        setIsCartOpen,
+    } from "../../state";
+    import { useNavigate } from "react-router-dom";
 
-const FlexBox = styled(Box)`
-display: flex;
-justify-content: space-between;
-align-items: center;
-`;
+    const FlexBox = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    `;
 
-const CartMenu = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const cart = useSelector((state) => state.cart.cart)
-    const isCartOpen = useSelector((state) => state.cart.isCartOpen)
+    const CartMenu = () => {
+        const navigate = useNavigate()
+        const dispatch = useDispatch()
+        const cart = useSelector((state) => state.cart.cart)
+        const isCartOpen = useSelector((state) => state.cart.isCartOpen)
 
-    const totalPrice = cart.reduce((total, item) => {
-        return total + item.count * item.attributes.price;
-    }, 0);
+        const totalPrice = cart.reduce((total, item) => {
+            return total + item.count * item.attributes.price;
+        }, 0);
 
-  return (
-      <Box
-      display={isCartOpen ? 'black' : 'none'}
-      backgroundColor='rgba(0, 0, 0, 0.4)'
-      position='fixed'
-      zIndex={10}
-      width='100%'
-      height='100%'
-      left='0'
-      top='0'
-      overflow='auto'
-      >
+      return (
           <Box
+          display={isCartOpen ? 'black' : 'none'}
+          backgroundColor='rgba(0, 0, 0, 0.4)'
           position='fixed'
-          right='0'
-          bottom='0'
-          width='max(400px. 30%)'
+          zIndex={10}
+          width='100%'
           height='100%'
-          backgoundColor='white'
+          left='0'
+          top='0'
+          overflow='auto'
           >
 
+              {/*MODAL*/}
+
+              <Box
+              position='fixed'
+              right='0'
+              bottom='0'
+              width='max(400px. 30%)'
+              height='100%'
+              backgoundColor='white'
+              >
+                <Box padding='30px' overflow='auto' height='100%'>
+
+                {/* HEADER  */}
+
+                    <FlexBox mb='15px'>
+                        <Typography variant='h3'>SHOPPING BAG ({cart.length})</Typography>
+                        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                            <CloseIcon />
+                        </IconButton>
+                    </FlexBox>
+
+                {/*    CART LIST*/}
+                    <Box>
+                        {cart.map((item) => (
+                            <Box key={`${item.attributes.name}-${item.id}`}>
+                                <FlexBox p='15px 0'>
+                                    <Box flex='1 1 40%'>
+                                        <img
+                                            alt={item?.name}
+                                            width='123px'
+                                            height='164px'
+                                            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                                        />
+                                    </Box>
+                                    <Box flex='1 1 60%'>
+                                        {/*Item Name*/}
+                                        <FlexBox mb='5px'>
+                                            <Typography fontWeight='bold'>
+                                                {item.attributes.name}
+                                            </Typography>
+                                            <IconButton onClick={()=> dispatch(removeFromCart({id: item.id}))}>
+                                                <CloseIcon />
+                                            </IconButton>
+                                        </FlexBox>
+                                        <Typography>{item.attributes.shortDescription}</Typography>
+
+                                        {/*AMOUT*/}
+                                        <FlexBox m='15px 0'>
+                                            <Box
+                                            display='flex'
+                                            alignItems='center'
+                                            border={`1,5px solid ${shades.neutral[500]}`}
+                                            >
+
+                                                <IconButton
+                                                onClick={()=> dispatch(decreaseCount({id: item.id}))}
+                                                >
+                                                    <RemoveIcon />
+                                                </IconButton>
+                                                <Typography>{item.count}</Typography>
+
+                                                <IconButton
+                                                    onClick={()=> dispatch(increaseCount({id: item.id}))}
+                                                >
+                                                    <AddIcon />
+                                                </IconButton>
+                                            </Box>
+                                        </FlexBox>
+
+                                    {/*    PRICE*/}
+                                        <Typography fontWeight='bold'>
+                                            ${item.attributes.price}
+                                        </Typography>
+
+                                    </Box>
+                                </FlexBox>
+                                <Divider />
+                            </Box>
+                        ))}
+                    </Box>
+
+                {/* ACTIONS   */}
+                    <Box m='20px 0'>
+                        <FlexBox m='20px 0'>
+
+                        </FlexBox>
+                    </Box>
+                </Box>
+              </Box>
           </Box>
-      </Box>
-  )
-}
-        export default CartMenu
+      )
+    }
+            export default CartMenu
+
+
